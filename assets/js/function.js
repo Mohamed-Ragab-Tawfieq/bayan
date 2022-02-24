d24bab70b77a3c9a5ea89fc5e9675656e2c2be87
Mohamed Ragab <mohamedragab.cs@gmail.com>
2022-02-24 22:35:31 +0200

    navbar

--- assets/js/function.js
+++ assets/js/function.js
@@ -29,11 +29,18 @@ window.onload = function () {
             $('#areaTypeTxt').removeClass('d-none')
         })
 
-        // phone inputs
-        const phoneInputField = document.querySelector("#phone");
-        const phoneInput = window.intlTelInput(phoneInputField, {
-            initialCountry: "eg",
-            utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+
+        // navbar submenu
+        $(".trigger").mouseenter(function () {
+            $(this).next('.sub-menu').addClass('show');
+        });
+
+        $(".sub-menu").mouseenter(function () {
+            $(this).addClass('show');
+        });
+
+        $(".trigger").mouseleave(function () {
+            $(this).next('.sub-menu').removeClass('show');
         });
 
         (jQuery);
